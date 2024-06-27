2d1fa4057875edd903b92a18b419611c46376a87
soumadeep-dey <contactsoumadeepdey@gmail.com>
2024-06-28 01:47:24 +0530

    initial setup

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+// DB
+require("./config/db");
+// CORS
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Router
+const taskRoutes = require("./routes/TaskRoutes");
+app.use("/tasks", taskRoutes);
+
+// Listen
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`🛜  Server running on port: ${PORT}`);
+});
