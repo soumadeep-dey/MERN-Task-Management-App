3d77a96ee912d08155bf583a8bd83b486d153a4d
soumadeep-dey <contactsoumadeepdey@gmail.com>
2024-06-28 01:47:39 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TaskManager from './components/TaskManager'
+
+const App = () => {
+  return (
+    <>
+      <TaskManager/>
+    </>
+  )
+}
+
+export default App
